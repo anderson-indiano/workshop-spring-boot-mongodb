183a0edc593ffce63ec0852d7553d11fc09b514f
anderson-indiano <indianobox2@gmail.com>
2021-05-07 18:02:16 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/indiano/workshopmongo/controller/UserController.java
+++ src/main/java/com/indiano/workshopmongo/controller/UserController.java
@@ -44,10 +44,32 @@ public class UserController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+		
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User user = userService.fromDTO(userDTO);
+		user.setId(id);
+		user = userService.update(user);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void>  delete(@PathVariable String id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build(); //Code response: 204
 	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
