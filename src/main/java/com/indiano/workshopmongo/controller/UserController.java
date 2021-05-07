023e0e8280cdc8516f353a12256c38aed3f9d181
anderson-indiano <indianobox2@gmail.com>
2021-05-07 15:40:28 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/indiano/workshopmongo/controller/UserController.java
+++ src/main/java/com/indiano/workshopmongo/controller/UserController.java
@@ -44,4 +44,10 @@ public class UserController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void>  delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build(); //Code response: 204
+	}
 }
