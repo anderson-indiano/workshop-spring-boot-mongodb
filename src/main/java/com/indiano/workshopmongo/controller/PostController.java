3329bec38fe565d911e564d3da0df7669d8bc8fb
anderson-indiano <indianobox2@gmail.com>
2021-05-08 16:07:58 -0300

    Consulta com varios criterios

--- src/main/java/com/indiano/workshopmongo/controller/PostController.java
+++ src/main/java/com/indiano/workshopmongo/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.indiano.workshopmongo.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,17 @@ public class PostController {
 		return ResponseEntity.ok().body(posts);
 	}
 	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());		
+		List<Post> posts = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(posts);
+	}	
 }
 
 
