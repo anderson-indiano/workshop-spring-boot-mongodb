4d7731b21c34356354a499d520d31a6b6160e2b2
anderson-indiano <indianobox2@gmail.com>
2021-05-08 13:33:02 -0300

    Referenciando os posts do usuario

--- src/main/java/com/indiano/workshopmongo/domain/User.java
+++ src/main/java/com/indiano/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.indiano.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -22,4 +25,7 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	final private List<Post> posts = new ArrayList<>();
 }
